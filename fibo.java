90f90625e846c50d25ae70af7e4aefc34c69a8e1
Sushantfox <115439873+Sushantfox@users.noreply.github.com>
2022-10-11 18:36:48 +0530

    Create fibo.java

---
+++ fibo.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class FibSeries {
+
+ public static void main(String[] args) {
+  int FibLength;
+  Scanner sc = new Scanner(System.in); //create object
+
+  System.out.print("Please enter length: ");
+  FibLength = sc.nextInt();
+
+  int[] num = new int[FibLength];
+  //initialized first element to 0
+  num[0] = 0;
+  //initialized second element to 1
+  num[1] = 1;
+  //help
+  }
+ }
+
+}
